c050c223a56f2377e9a3668f95779264035dea0e
Pierre Bouillon <pierre.bouillon@openmailbox.org>
2018-01-19 09:09:26 +0100

    added base view

--- BasicGallery/app/src/main/java/com/example/pierrebouillon/basicgallery/MainActivity.java
+++ BasicGallery/app/src/main/java/com/example/pierrebouillon/basicgallery/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.pierrebouillon.basicgallery;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonExit = findViewById(R.id.butExit) ;
+        Button buttonTake = findViewById(R.id.butTake) ;
+
+        buttonExit.setOnClickListener( v -> {
+            finish() ;
+        }) ;
+
+        buttonTake.setOnClickListener( v -> {
+
+        });
     }
 }
